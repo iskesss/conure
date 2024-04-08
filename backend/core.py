d11f7b571151ca59b6eba40d732493b16c282cc2
Jordan Bouret <JordanLucBouret@gmail.com>
2024-04-07 19:04:32 -0700

    Implemented conversational memory

--- backend/core.py
+++ backend/core.py
@@ -1,13 +1,13 @@
 import os
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain_pinecone import Pinecone
 
 print("hello world")
 
 
-def run_llm(myQuery: str) -> any:
+def run_llm(myQuery: str, chatHistory: list[(str, any)] = []) -> any:
 
     # from_existing_index ALSO takes index_name as an argument, but I'm pretty sure it'll automatically load that from my environmental variables
     docsearch = Pinecone.from_existing_index(
@@ -19,13 +19,20 @@ def run_llm(myQuery: str) -> any:
     # EXPLANATION OF THE THIRD ARGUMENT BELOW:
     # a retriever is simply a wrapper around our vectorstore object which allows us to go retrieve relevant info through similarity search.
     # all vectorstore objects (even non-Pinecone ones) can become retriever objects. just use .as_retriever(), which wraps the vectorstore upon which it was called and wraps it in the retriever class
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+    # qa = RetrievalQA.from_chain_type(
+    #     llm=llm,
+    #     chain_type="stuff",
+    #     retriever=docsearch.as_retriever(),
+    #     return_source_documents=True,
+    # )
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": myQuery})  # plug myQuery into our question/answer chain
+
+    return qa(
+        {"question": myQuery, "chat_history": chatHistory}
+    )  # plug myQuery into our question/answer chain
 
 
 # myQuery = "What is a Langchain chain?"
--- main.py
+++ main.py
@@ -6,14 +6,35 @@ from streamlit_chat import message
 st.header("Conure 🦜 a squawking good Langchain documentation assistant")
 
 user_prompt = st.text_input(
-    label="Provide a Prompt", placeholder="Enter your question here..."
+    label="Provide a Prompt",
+    placeholder="Enter your question here...",
 )
 
-if "user_prompt_history" not in st.session_state:
+if "user_prompt_history" not in st.session_state:  # initializing
     st.session_state["user_prompt_history"] = []
 
-if "chat_answers_history" not in st.session_state:
+if "chat_answers_history" not in st.session_state:  # initializing
     st.session_state["chat_answers_history"] = []
+
+if "chat_history" not in st.session_state:  # initializing
+    st.session_state["chat_history"] = []
+
+
+# there is certainly a more succinct way to do this with Python but i'd rather just do it manually
+def truncate_https(link: str) -> str:
+    output = ""
+    counter = 0
+    char_index = 0
+    while char_index < len(link) and counter < 2:
+        if link[char_index] == "/":
+            counter += 1
+        char_index += 1
+
+    while char_index < len(link):
+        output += link[char_index]
+        char_index += 1
+
+    return output
 
 
 def create_sources_string(source_urls: set[str]) -> str:
@@ -23,25 +44,30 @@ def create_sources_string(source_urls: set[str]) -> str:
     sources_list.sort()
     sources_string = "Sources:\n"
     for i, source in enumerate(sources_list):
-        sources_string += f"{i+1}. {source}\n"
+        sources_string += f"{i+1}. {truncate_https(source)}\n"
     return sources_string
 
 
 if user_prompt:
     with st.spinner("Generating Response..."):
-        generated_response = run_llm(user_prompt)
+        generated_response = run_llm(user_prompt, st.session_state["chat_history"])
 
         # we make the sources variable a set so that duplicate sources are not included
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
+        # print(generated_response)
+
         parsed_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         st.session_state["user_prompt_history"].append(user_prompt)
         st.session_state["chat_answers_history"].append(parsed_response)
+        st.session_state["chat_history"].append(
+            (user_prompt, generated_response["answer"])
+        )  # append a tuple
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
